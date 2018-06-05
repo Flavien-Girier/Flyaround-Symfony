4b744fdf49f81278b419e2545e563dd6f3872cda
OWSohaku <girier.flavien@gmail.com>
2018-06-05 14:12:18 +0200

    WebPack Encore Solution

--- var/cache/dev/twig/70/70387a2492afbf915bf6dabfe4f14637fef37dbae1d81b3b0bae69f44c040bdb.php
+++ var/cache/dev/twig/70/70387a2492afbf915bf6dabfe4f14637fef37dbae1d81b3b0bae69f44c040bdb.php
@@ -47,44 +47,31 @@ class __TwigTemplate_27153992e2224efaf1fa3012d5d6df1177990fa9830c6e03345ae0db708
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 4
-        echo "    <div id=\"wrapper\">
-        <div id=\"container\">
-            <h1>Routes disponibles :</h1>
-            <ul>
-                <li>
-                    <a href=\"";
-        // line 9
-        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("planemodel_index");
-        echo "\">Voir les avions</a>
-                </li>
-                <li>
-                    <a href=\"";
-        // line 12
-        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("reservation_index");
-        echo "\">Voir les reservations</a>
-                </li>
-                <li>
-                    <a href=\"";
-        // line 15
+        echo "    <nav>
+            <ul class=\"nav\">
+                <a class=\"nav-item\" href=\"";
+        // line 6
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("flight_index");
         echo "\">Voir les vols</a>
-                </li>
-                <li>
-                    <a href=\"";
-        // line 18
+                <a class=\"nav-item\" href=\"";
+        // line 7
+        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("reservation_index");
+        echo "\">Voir les reservations</a>
+                <a class=\"nav-item\" href=\"";
+        // line 8
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("site_index");
         echo "\">Voir les terrains</a>
-                </li>
-                <li>
-                    <a href=\"";
-        // line 21
+                <a class=\"nav-item\" href=\"";
+        // line 9
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("review_index");
         echo "\">Voir tous les avis utilisateurs</a>
-                </li>
-
+                <a class=\"nav-item\" href=\"";
+        // line 10
+        echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("planemodel_index");
+        echo "\">Voir les avions</a>
             </ul>
-        </div>
-    </div>
+    </nav>
+
 ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -94,7 +81,7 @@ class __TwigTemplate_27153992e2224efaf1fa3012d5d6df1177990fa9830c6e03345ae0db708
 
     }
 
-    // line 29
+    // line 16
     public function block_stylesheets($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
@@ -103,33 +90,8 @@ class __TwigTemplate_27153992e2224efaf1fa3012d5d6df1177990fa9830c6e03345ae0db708
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
 
-        // line 30
-        echo "<style>
-    body { background: #F5F5F5; font: 18px/1.5 sans-serif; }
-    h1, h2 { line-height: 1.2; margin: 0 0 .5em; }
-    h1 { font-size: 36px; }
-    h2 { font-size: 21px; margin-bottom: 1em; }
-    p { margin: 0 0 1em 0; }
-    a { color: #0000F0; }
-    a:hover { text-decoration: none; }
-    code { background: #F5F5F5; max-width: 100px; padding: 2px 6px; word-wrap: break-word; }
-    #wrapper { background: #FFF; margin: 1em auto; max-width: 800px; width: 95%; }
-    #container { padding: 2em; }
-    #welcome, #status { margin-bottom: 2em; }
-    #welcome h1 span { display: block; font-size: 75%; }
-    #icon-status, #icon-book { float: left; height: 64px; margin-right: 1em; margin-top: -4px; width: 64px; }
-    #icon-book { display: none; }
-
-    @media (min-width: 768px) {
-        #wrapper { width: 80%; margin: 2em auto; }
-        #icon-book { display: inline-block; }
-        #status a, #next a { display: block; }
-
-        @-webkit-keyframes fade-in { 0% { opacity: 0; } 100% { opacity: 1; } }
-        @keyframes fade-in { 0% { opacity: 0; } 100% { opacity: 1; } }
-        .sf-toolbar { opacity: 0; -webkit-animation: fade-in 1s .2s forwards; animation: fade-in 1s .2s forwards;}
-    }
-</style>
+        // line 17
+        echo "
 ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -151,7 +113,7 @@ class __TwigTemplate_27153992e2224efaf1fa3012d5d6df1177990fa9830c6e03345ae0db708
 
     public function getDebugInfo()
     {
-        return array (  107 => 30,  98 => 29,  81 => 21,  75 => 18,  69 => 15,  63 => 12,  57 => 9,  50 => 4,  41 => 3,  11 => 1,);
+        return array (  94 => 17,  85 => 16,  70 => 10,  66 => 9,  62 => 8,  58 => 7,  54 => 6,  50 => 4,  41 => 3,  11 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -167,58 +129,20 @@ class __TwigTemplate_27153992e2224efaf1fa3012d5d6df1177990fa9830c6e03345ae0db708
         return new Twig_Source("{% extends 'base.html.twig' %}
 
 {% block body %}
-    <div id=\"wrapper\">
-        <div id=\"container\">
-            <h1>Routes disponibles :</h1>
-            <ul>
-                <li>
-                    <a href=\"{{ path('planemodel_index') }}\">Voir les avions</a>
-                </li>
-                <li>
-                    <a href=\"{{ path('reservation_index') }}\">Voir les reservations</a>
-                </li>
-                <li>
-                    <a href=\"{{ path('flight_index') }}\">Voir les vols</a>
-                </li>
-                <li>
-                    <a href=\"{{ path('site_index') }}\">Voir les terrains</a>
-                </li>
-                <li>
-                    <a href=\"{{ path('review_index') }}\">Voir tous les avis utilisateurs</a>
-                </li>
-
+    <nav>
+            <ul class=\"nav\">
+                <a class=\"nav-item\" href=\"{{ path('flight_index') }}\">Voir les vols</a>
+                <a class=\"nav-item\" href=\"{{ path('reservation_index') }}\">Voir les reservations</a>
+                <a class=\"nav-item\" href=\"{{ path('site_index') }}\">Voir les terrains</a>
+                <a class=\"nav-item\" href=\"{{ path('review_index') }}\">Voir tous les avis utilisateurs</a>
+                <a class=\"nav-item\" href=\"{{ path('planemodel_index') }}\">Voir les avions</a>
             </ul>
-        </div>
-    </div>
+    </nav>
+
 {% endblock %}
 
 {% block stylesheets %}
-<style>
-    body { background: #F5F5F5; font: 18px/1.5 sans-serif; }
-    h1, h2 { line-height: 1.2; margin: 0 0 .5em; }
-    h1 { font-size: 36px; }
-    h2 { font-size: 21px; margin-bottom: 1em; }
-    p { margin: 0 0 1em 0; }
-    a { color: #0000F0; }
-    a:hover { text-decoration: none; }
-    code { background: #F5F5F5; max-width: 100px; padding: 2px 6px; word-wrap: break-word; }
-    #wrapper { background: #FFF; margin: 1em auto; max-width: 800px; width: 95%; }
-    #container { padding: 2em; }
-    #welcome, #status { margin-bottom: 2em; }
-    #welcome h1 span { display: block; font-size: 75%; }
-    #icon-status, #icon-book { float: left; height: 64px; margin-right: 1em; margin-top: -4px; width: 64px; }
-    #icon-book { display: none; }
 
-    @media (min-width: 768px) {
-        #wrapper { width: 80%; margin: 2em auto; }
-        #icon-book { display: inline-block; }
-        #status a, #next a { display: block; }
-
-        @-webkit-keyframes fade-in { 0% { opacity: 0; } 100% { opacity: 1; } }
-        @keyframes fade-in { 0% { opacity: 0; } 100% { opacity: 1; } }
-        .sf-toolbar { opacity: 0; -webkit-animation: fade-in 1s .2s forwards; animation: fade-in 1s .2s forwards;}
-    }
-</style>
 {% endblock %}
 ", "default/index.html.twig", "/home/wilder/WCS/Quête/FlyAround/app/Resources/views/default/index.html.twig");
     }
